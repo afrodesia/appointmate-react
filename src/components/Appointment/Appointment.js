ff07cce623c949a44cbf2479f4b40689e29c29a1
Ken Torry <kentorry@afrodesiamedia.com>
2016-12-23 03:40:27 -0600

    imported jquery and moved data.json file to public and modifed Appointement.js

--- src/components/Appointment/Appointment.js
+++ src/components/Appointment/Appointment.js
@@ -1,65 +1,43 @@
 import React from 'react'
+import $ from 'jquery'
 import './appointment.css'
 
 
 const Appointment = React.createClass({
+
         getInitialState: function() {
             return {
-                data: [
-                    {
-                        "name": "Ken Torry",
-                        "date": "November 23, 2016",
-                        "time": "1:00pm",
-                        "service": "Flyer",
-                        "notes": "Some dummy text right here for notes"
-                    },
-                    {
-                        "name": "Dave Chappel",
-                        "date": "November 23, 2016",
-                        "time": "1:50pm",
-                        "service":"Logo Design",
-                        "notes": "Some dummy text right here for notes"
-                    },
-                    {
-                        "name": "Patty Mills",
-                        "date": "November 28, 2016",
-                        "time": "3:00pm",
-                        "service":"Logo design",
-                        "notes": "Some dummy text right here for notes"
-                    },
-                    {
-                        "name": "Mark Torres",
-                        "date": "December 3, 2016",
-                        "time": "11:00am",
-                        "service":"Web Development",
-                        "notes": "Some dummy text right here for notes"
-                    },
-                    {
-                        "name": "Liz Paterson",
-                        "date": "December 4, 2016",
-                        "time": "1:00pm",
-                        "service": "HTML Email",
-                        "notes": "Some dummy text right here for notes"
-                    }
-                ]
+                myAppointments: []
             }
         },
+        componentDidMount: function() {
+            this.serverRequest = $.get('./data/data.json', function(result){
+                var tempApts = result
+                this.setState({
+                    myAppointments : tempApts
+                })
+            }.bind(this))
+        },
+        componentWillUnmount: function() {
+            this.serverRequest.abort()
+        },
         render: function() {
-            let filteredApts = this.state.data
-            filteredApts = filteredApts.map(function(item, index){
+            let filteredApts = this.state.myAppointments
+            filteredApts = filteredApts.map(function(item, index) {
                 return(
                     <li className="info media-body" key={index}>
                         <div className="info-head">
-                            <span className="info-time pull-right">{this.state.data[index].time}</span>
-                            <h3 className="info-name">{this.state.data[index].name}</h3>
-                            <p className="info-date">{this.state.data[index].date}</p>
+                            <span className="info-time pull-right">{this.state.myAppointments[index].time}</span>
+                            <h3 className="info-name">{this.state.myAppointments[index].name}</h3>
+                            <p className="info-date">{this.state.myAppointments[index].date}</p>
                             <hr/>
-                            <p className="info-notes">{this.state.data[index].notes}</p>
-                            <p className="info-service pull-right">{this.state.data[index].service}</p>
+                            <p className="info-notes">{this.state.myAppointments[index].notes}</p>
+                            <p className="info-service pull-right">{this.state.myAppointments[index].service}</p>
                         </div>
                     </li>
                 )
             }.bind(this))
+
             return (
                 <section className="appointment container">
                     <h2>Appointments</h2>
