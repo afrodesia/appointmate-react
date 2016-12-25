e987f61579083991d320d1fbfe7fc6d5102161fb
Ken Torry <kentorry@afrodesiamedia.com>
2016-12-24 23:39:22 -0600

    minor css fix

--- src/components/Appointment/AddAppointment.js
+++ src/components/Appointment/AddAppointment.js
@@ -7,9 +7,9 @@ import './appointment.css'
 const AddAppointment = React.createClass({
         render: function() {
             return( 
-                <section className="panel panel-primay">
+                <div className="panel panel-primay add-appointment">
                     <h2><i className="fa fa-plus-circle" aria-hidden="true"></i> Add Appointment</h2>
-                    <form className="add-appointment">
+                    <form className="">
                         <div className="form-group">
                             <label for="lName">First Name</label>
                             <input id="msg" type="text" className="form-control" 
@@ -26,18 +26,24 @@ const AddAppointment = React.createClass({
                             <label for="time">Time</label>
                             <input id="msg" type="text" className="form-control" 
                             name="msg" placeholder="" />
-                        </div> 
+                        </div>
+
+                        <div className="form-group">
+                            <label for="service">Service Needed</label>
+                            <input id="msg" type="text" className="form-control" 
+                            name="msg" placeholder="" />
+                        </div>  
 
                         <div className="form-group">
                             <label for="ANote">Add your notes</label>
                             <textarea id="msg" type="text" className="text-box form-control" 
                             name="msg" placeholder=""> </textarea>
-                        </div> 
-
-                        <button type="button" className="btn btn-danger ">Add Appointment</button>
-
+                        </div>
+                        <div className="form-group"> 
+                            <button type="button" className="btn btn-danger ">Add Appointment</button>
+                        </div>
                     </form>
-                </section>
+                </div>
 
        )
     }
